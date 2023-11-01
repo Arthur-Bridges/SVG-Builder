852c1c0fcd6b2f6151b613eb306e09db4884bef9
Arthur Bridges <arthurbridges08@yahoo.com>
2023-11-01 00:59:24 -0500

    trying to debug

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 import inquirer from 'inquirer';
 import {writeFile} from 'fs/promises';
-import {Square, Circle, Triangle} from './lib/shapes';
+import {Square, Circle, Triangle} from '../lib/shapes.js';
+
+class SvgFile {
+    constructor(){
+        this.textElement = '';
+        this.shapeElement = '';
+    }
+
+    render() {
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+      }
+    
+      setTextElement = (text, color) => {
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+      }
+    
+      setShapeElement = (shape) => {
+        this.shapeElement = shape.render();
+      }
+}
 
 const questions = [
     {
@@ -26,25 +45,6 @@ const questions = [
     },
   ];
 
-class SvgFile {
-    constructor(){
-        this.textEl = '';
-        this.shapeEl = '';
-    }
-
-    render() {
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
-      }
-    
-      setTextElement = (text, color) => {
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
-      }
-    
-      setShapeElement = (Shapes) => {
-        this.shapeElement = Shapes.render();
-      }
-}
-
 const writeToFile = async (fileName, data) => {
     console.log(`Writing [${data}] to file [${fileName}]`);
     try {
@@ -68,15 +68,15 @@ const writeToFile = async (fileName, data) => {
     switch (shapeImg.toLowerCase()) {
 
       case 'square':
-        userShape = new squareRen();
+        userShape = new Square();
         break;
 
       case 'circle':
-        userShape = new circleRen();
+        userShape = new Circle();
         break;
       
       case 'triangle':
-        userShape = new triangleRen();
+        userShape = new Triangle();
         break;
 
       default:
