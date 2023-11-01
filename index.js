31330c3e5417fbc2f71f424e197b8b82103047e8
Arthur Bridges <arthurbridges08@yahoo.com>
2023-11-01 00:06:34 -0500

    fixing errors

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ class SvgFile {
         this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
       }
     
-      setShapeElement = (shape) => {
-        this.shapeElement = shape.render();
+      setShapeElement = (Shapes) => {
+        this.shapeElement = Shapes.render();
       }
 }
 
